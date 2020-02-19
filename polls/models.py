ee3187a8fc83904d4eb7af01586b3ef1b433639a
Sanek0125 <a.p.primakov@gmail.com>
2020-02-19 23:16:40 +0700

    Migrate BD SQliite models question and choice

--- polls/models.py
+++ polls/models.py
@@ -4,12 +4,12 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Question(models.Model): # Создаем необходимый класс
+    question_text = models.CharField(max_length=200) # Создаем 2 поля Вопрос и Дату публикации
     pub_date = models.DateTimeField('date published')
-
+# CharField - поле из символов, DateTimeField - поле даты и времени
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # Вопрос из класса Question
+    choice_text = models.CharField(max_length=200) # max_length - обязательное поле
+    votes = models.IntegerField(default=0) # Кол-во голосов По умолчанию 0
